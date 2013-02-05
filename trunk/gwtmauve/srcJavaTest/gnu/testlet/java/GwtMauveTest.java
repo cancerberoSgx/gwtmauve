d5368f983fd0aace582eab1030adae230bf29064
sebastigurin <sebastigurin@d596cc32-6bd5-48a5-c752-6a991be30df2>
2013-02-05 16:52:10 +0000

    java.lang all tests

--- trunk/gwtmauve/srcJavaTest/gnu/testlet/java/GwtMauveTest.java
+++ trunk/gwtmauve/srcJavaTest/gnu/testlet/java/GwtMauveTest.java
@@ -1,6 +1,8 @@
 package gnu.testlet.java;
 
 import gnu.testlet.java.lang.Double.DoubleTest;
+import gnu.testlet.java.report.JavaLangTests;
+import gnu.testlet.java.report.Tester;
 
 import com.google.gwt.core.client.EntryPoint;
 
@@ -8,11 +10,17 @@ public class GwtMauveTest implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-		System.out.println("jaja");
 		
 		DoubleTest t1 = new DoubleTest();
 		TestHarnessImpl h = new TestHarnessImpl(); 
 		t1.test(h); 
+		
+		JavaLangTests javaLangTests = new JavaLangTests(); 
+		
+		Tester tester = new Tester(javaLangTests.getTests(), "");
+		String result = tester.buildHTMLReport();
+		System.out.println(result);
+		System.out.println("end");
 		
 	}
 
